00a992d828fc6fc3644f3d929314e3ed42d55ae5
Christoph Zobel <Zobel@klara-oppenheimer-schule.de>
2022-10-14 12:03:09 +0200

    Beispiel Fegen

---
+++ Module Client-Server/QAAufgabe/Beispiel Fegen/Lehrling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SD_Übungen
+{
+    class Lehrling
+    {
+
+        public void fegeWerkstatt()
+        {
+            Console.WriteLine("Jahwohl");
+            this.fegtWerkstatt();
+            Console.WriteLine("Fertig");
+        }
+
+        private void fegtWerkstatt()
+        {
+            Console.WriteLine("Feg, feg, feg");
+        }
+    }
+}
---
+++ Module Client-Server/QAAufgabe/app.js
@@ -0,0 +1,3 @@
+let x= 19.00 + "Hallo 12. Klasse" +4
+console.log(typeof(x))
+console.log(x)
