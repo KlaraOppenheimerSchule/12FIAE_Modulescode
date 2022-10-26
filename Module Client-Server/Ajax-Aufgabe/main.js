1e761d5c8237d01488b6ab794c9bea77d9395e77
StHinz <39180199+StHinz@users.noreply.github.com>
2022-10-26 22:25:59 +0200

    Update main.js
    delete promise

--- Module Client-Server/Ajax-Aufgabe/main.js
+++ Module Client-Server/Ajax-Aufgabe/main.js
@@ -2,16 +2,6 @@ const outputElement = document.querySelector('.output');
 const buttonElement = document.querySelector('button');
 
 
-let promise = new Promise(function (resolve, reject) {
-  let x = 0;
-
-  // The producing code (this may take some time)
-  if (x == 0) {
-    resolve('OK');
-  } else {
-    reject('Error');
-  }
-}); 
 
 async function getCatFact() {
 
