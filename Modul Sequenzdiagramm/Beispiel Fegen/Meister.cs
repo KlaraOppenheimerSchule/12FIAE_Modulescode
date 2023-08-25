e562858aea7375350481f8fe6f67a1d94ee85e85
Hinz <hinz@klara-oppenheimer-schule.de>
2023-08-25 11:03:08 +0200

    update SEQ

---
+++ Modul Sequenzdiagramm/Beispiel Fegen/Meister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SD_Übungen
+{
+    class Meister
+    {
+        public Meister(Lehrling eins)
+        {
+            Console.WriteLine("Hey, fege die Werkstatt!");
+            eins.fegeWerkstatt();
+            
+        }
+    }
+}
