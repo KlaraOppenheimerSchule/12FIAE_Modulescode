979309fc4dff77d198a27dace525453ff757fc09
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2024-02-22 20:20:18 +0100

    Cleaning up

---
+++ Modul Sequenzdiagramm/Fragmente/meister-DESKTOP-UUQD9AI.py
@@ -0,0 +1,32 @@
+from stuhl import stuhl
+
+class meister:
+    def __init__(self):
+        self.__laune = True
+        self.__stuehle = []
+       
+    def setLaune(self,laune):
+        self.__laune = laune
+    
+    def getLaune(self):
+        return self.__laune
+  
+    def addStuhl(self, gebauterStuhl : stuhl):
+        self.__stuehle.append(gebauterStuhl)
+    
+    def getStuhlQualitaet(self, index):
+        return self.__stuehle[index].getQualitaet()
+    
+    def anwtortAufPauseGeben(self):
+        antwort = "Wir gehen Eis essen!"
+        return antwort
+     
+            
+    def pruefeArbeit(self, index):
+        
+        qualitaet = self.__stuehle[index].getQualitaet()
+        
+        if (qualitaet == True):
+            print("Gute Arbeit")
+        else:
+            print("Mach es noch mal!")
