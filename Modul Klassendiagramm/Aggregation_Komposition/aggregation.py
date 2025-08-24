1d1b5eafad9f96d75a475c50ebd26b089f248b6f
StHinz <39180199+StHinz@users.noreply.github.com>
2025-08-24 10:36:44 +0200

    Add Aggreagtion and Compisition

---
+++ Modul Klassendiagramm/Aggregation_Komposition/aggregation.py
@@ -0,0 +1,55 @@
+from pickle import FALSE, NONE
+
+class spieler():
+       
+    
+#constructor
+    def __init__(self, spielername, konto):
+        self.__spielername = spielername
+        self.__konto = konto
+        
+#getName
+    def getName(self):
+        return self.__spielername
+  
+# Class Strasse
+class strasse():
+     
+    #constructor
+    def __init__(self, strassenname, miete):
+        self.__strassenname = strassenname
+        self.__miete = miete
+        self.__besitzer = [spieler] = NONE
+    
+    #getBesitzer
+    def getBesitzer(self):
+        if self.__besitzer is NONE:
+            return 'Die Strasse hat keinen Besitzer'
+        else:
+            return self.__besitzer.getName()
+    
+    
+    #addBesitzer
+    def addBesitzer(self, besitzer):
+        self.__besitzer = besitzer
+        
+      
+    #Remove Besitzer      
+    def removeBesitzer(self):
+        self.__besitzer = NONE
+
+# Test   
+sp1 = spieler('Max', 100.00)
+st1 = strasse('Badstrasse',1000.00)
+
+# Besitzer hinzufügen
+st1.addBesitzer(sp1)
+
+# Besitzer ausgeben lassen
+print(st1.getBesitzer())
+
+# Besitzer entfernen
+st1.removeBesitzer()
+
+# Besitzer ausgeben lassen
+print(st1.getBesitzer())
---
+++ Modul Klassendiagramm/Aggregation_Komposition/compisiton.py
@@ -0,0 +1,44 @@
+from pickle import FALSE, NONE
+
+class spieler():
+       
+    
+#constructor
+    def __init__(self, spielername, konto):
+        self.__spielername = spielername
+        self.__konto = konto
+        
+#getName
+    def getName(self):
+        return self.__spielername
+  
+# Class Strasse
+class strasse():
+     
+    #constructor
+    def __init__(self, strassenname, miete, spieler):
+        if not spieler:
+            raise ValueError("Objekterstellung nicht erlaubt.")
+            print("Objekt wird nicht erzeugt!")
+
+        else:
+            self.__strassenname = strassenname
+            self.__miete = miete
+            self.__besitzer = spieler
+            print("Objekt wird erzeugt!")
+    
+    #getBesitzer
+    def getBesitzer(self):
+        return self.__besitzer.getName()
+    
+
+
+try:
+    st1 = strasse('Badstrasse',1000.00, spieler('Max', 100.00))
+    st2 = strasse('Badstrasse',1000.00, None)
+except ValueError as e:
+    print(e)
+
+
+
+
